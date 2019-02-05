4c4870cbe8c76484752b14f2b86deb3293c7b977
Sankhadeep Roy <sankhadeeproy007@gmail.com>
2019-02-05 15:02:17 +0530

    chore(): adds cloud functions

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,14 +1,14 @@
 export const createProject = project => {
   return (dispatch, getState, { getFirestore }) => {
     const firestore = getFirestore();
-    const profile = getState().firebase.profile;
+    // const profile = getState().firebase.profile;
     const authorId = getState().firebase.auth.uid;
     firestore
       .collection('projects')
       .add({
         ...project,
-        authorFirstName: profile.firstName,
-        authorLastName: profile.lastName,
+        authorFirstName: 'Name',
+        authorLastName: 'Last',
         authorId: authorId,
         createdAt: new Date()
       })
